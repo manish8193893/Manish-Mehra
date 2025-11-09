ad5834c678c412a2e89350241c57e57dd2b6e4b7
Manish Kumar <manish8193892@gmail.com>
2025-11-10 02:19:50 +0530

    Update footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,5 +1,5 @@
-
-import { Facebook, Twitter, Instagram, Linkedin, Github } from 'lucide-react';
+import React from "react";
+import { Phone, Mail, MessagesSquare, Github } from "lucide-react";
 
 const Footer = () => {
   const currentYear = new Date().getFullYear();
@@ -10,13 +10,32 @@ const Footer = () => {
         <div className="border-t border-white/10 pt-8">
           <div className="flex flex-col md:flex-row justify-between items-center">
             <p className="text-gray-400 text-sm mb-4 md:mb-0">
-              &copy; {currentYear} CryptoFlow. All rights reserved.
-              ~ Distributed By <a href="https://themewagon.com/" target="_blank" className="text-crypto-purple hover:underline">ThemeWagon</a>
+              &copy; {currentYear} Personal Portfolio.&nbsp;&nbsp;All rights reserved.&nbsp;&nbsp;&nbsp;&nbsp;
+              ~ Designed and Developed by Manish Kumar
             </p>
             <div className="flex space-x-6">
-              <a href="#!" className="text-gray-400 hover:text-crypto-purple text-sm transition-colors">Terms of Service</a>
-              <a href="#!" className="text-gray-400 hover:text-crypto-purple text-sm transition-colors">Privacy Policy</a>
-              <a href="#!" className="text-gray-400 hover:text-crypto-purple text-sm transition-colors">Cookie Policy</a>
+              <ul className="flex items-center space-x-2">
+                <li className='p-2 border-gray-700 bg-white/20 rounded-md'>
+                  <a className="text-gray-300 transition-colors rounded-md border-b-2 border-transparent hover:border-white pb-1" href="tel:+918742970516">
+                    <Phone />
+                  </a>
+                </li>
+                <li className='p-2 border-gray-700 bg-white/20 rounded-md'>
+                  <a className="text-gray-300 transition-colors rounded-md border-b-2 border-transparent hover:border-white pb-1" href="mailto:manish8193892@gmail.com">
+                    <Mail />
+                  </a>
+                </li>
+                <li className='p-2 border-gray-700 bg-white/20 rounded-md'>
+                  <a className="text-gray-300 transition-colors rounded-md border-b-2 border-transparent hover:border-white pb-1" href="https://wa.me/+918742970516">
+                    <MessagesSquare />
+                  </a>
+                </li>
+                <li className='p-2 border-gray-700 bg-white/20 rounded-md'>
+                  <a className="text-gray-300 transition-colors rounded-md border-b-2 border-transparent hover:border-white pb-1" href="https://github.com/manish8193893">
+                    <Github />
+                  </a>
+                </li>
+              </ul>
             </div>
           </div>
         </div>
